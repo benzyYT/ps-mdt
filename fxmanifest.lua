242a181aa9a6c0b676e3168c97c6c1570a4f1b8b
Lexinor <alternonolimit@gmail.com>
2023-12-13 22:35:59 +0100

    Correction pb

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -25,8 +25,8 @@ server_scripts {
 client_scripts{
     'client/main.lua',
     'client/cl_impound.lua',
-    'client/cl_mugshot.lua'
-    'client/camera.lua'
+    'client/cl_mugshot.lua',
+    'client/camera.lua',
 } 
 
 ui_page 'ui/dashboard.html'
